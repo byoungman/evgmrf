e41f81e4d090b50b70ec26e63c804fa2e3a7761b
byoungman <45686107+byoungman@users.noreply.github.com>
2026-04-10 17:08:20 +0100

    Update gev_gmrf.cpp

--- src/gev_gmrf.cpp
+++ src/gev_gmrf.cpp
@@ -40,7 +40,7 @@ for (int i=0; i < n; i++) {
     y = yv(k);
     w = wv(k);
     
-    if(arma::is_finite(y)) {
+    if(std::isfinite(y)) {
       
       ee1 = xi * (y - mu) / exp(lpsi);
       
@@ -66,309 +66,6 @@ for (int i=0; i < n; i++) {
 return(nllh);
 
 }
-
-// // [[Rcpp::export(.tgevgmrfld1)]]
-// arma::vec tgevgmrfld1(const arma::mat& pars, const arma::field<arma::vec>& yc)
-// {
-// 
-// int n = yc.n_rows; // number of locations
-// int m;
-// 
-// double y, mu, lpsi, txi;
-// 
-// double ee2, ee3, ee4, ee5, ee6, ee7, ee9;
-// double ee10, ee11, ee12, ee13, ee14, ee15, ee16, ee17, ee18;
-// double ee20, ee21, ee22, ee23, ee25, ee27, ee28, ee29;
-// double ee30, ee31, ee33, ee34;
-// 
-// arma::uvec id(3);
-// id(0) = 0;
-// id(1) = n;
-// id(2) = 2 * n;
-// arma::vec g(n * 3, arma::fill::zeros);
-// arma::vec yv;
-// 
-// for (int i=0; i < n; i++) {
-// 
-//   mu = pars(0, i);
-//   lpsi = pars(1, i);
-//   txi = pars(2, i);
-//   
-//   m = yc(i).size(); // number of obs
-//   yv = yc(i);
-// 
-//   for (int k=0; k < m; k++) {
-// 
-//     y = yv(k);
-//     
-//     if(arma::is_finite(y)) {
-// 
-//     ee2 = exp(-txi);
-//     ee3 = 1 + ee2;
-//     ee4 = 1.5/ee3;
-//     ee5 = ee4 - 1;
-//     ee6 = exp(lpsi);
-//     ee7 = y - mu;
-//     ee9 = ee5 * ee7/ee6;
-//     ee10 = ee9 + 1;
-//     ee11 = 1/ee5;
-//     ee12 = R_pow(ee10, ee11);
-//     ee13 = 1 + ee11;
-//     ee14 = ee10 * ee6;
-//     ee15 = R_pow(ee3, 2);
-//     ee16 = log1p(ee9);
-//     ee17 = 1/ee12;
-//     ee18 = R_pow(ee10, ee13);
-//     ee20 = ee3 * ee5;
-//     ee21 = 1 + ee17;
-//     ee22 = 1.5 * (ee16/(ee12 * ee5));
-//     ee23 = 1.5/ee12;
-//     ee25 = (((ee23 - 1.5 * ee5) * ee7/ee14 + 1.5) * ee13 - (1.5 +  ee22)/ee5)/ee10 * ee2;
-//     ee27 = ((4.5/ee20 - 3) * ee2/ee3 + 1.5) * ee16;
-//     ee28 = ee13 * ee5;
-//     ee29 = ee13 * ee7;
-//     ee30 = ee15 * ee6;
-//     ee31 = R_pow(ee5, 2);
-//     ee33 = 1.5 * (ee7/(ee18 * ee6));
-//     ee34 = ee4 - ee21;
-//     
-//     g(i) += -((ee28 - ee17)/ee14);
-//     g(i + n) += (ee17 - ee28) * ee7/ee14 + 1;
-//     g(i + 2 * n) += (((ee23 - 1.5) * ee16/ee5 - ee33)/ee5 + 1.5 * (ee29/ee14)) * ee2/ee15;
-// 
-//     // g(id[0]) += -((ee28 - ee17)/ee14);
-//     // g(id[1]) += (ee17 - ee28) * ee7/ee14 + 1;
-//     // g(id[2]) += (((ee23 - 1.5) * ee16/ee5 - ee33)/ee5 + 1.5 * (ee29/ee14)) * ee2/ee15;
-//     
-//     }
-//     
-//   }
-// 
-//   id += 1;
-// 
-// }
-// 
-// return g;
-// 
-// }
-// 
-// // [[Rcpp::export(.tgevgmrfld2)]]
-// arma::sp_mat tgevgmrfld2(const arma::mat& pars, const arma::field<arma::vec>& yc)
-// {
-// 
-// int n = yc.n_rows; // number of locations
-// int m;
-//   
-// arma::sp_mat H(n * 3, n * 3);
-// 
-// double y, mu, lpsi, txi;
-// 
-// double ee2, ee3, ee4, ee5, ee6, ee7, ee9;
-// double ee10, ee11, ee12, ee13, ee14, ee15, ee16, ee17, ee18;
-// double ee20, ee21, ee22, ee23, ee25, ee27, ee28, ee29;
-// double ee30, ee31, ee33, ee34;
-// 
-// arma::uvec id(3);
-// id(0) = 0;
-// id(1) = n;
-// id(2) = 2 * n;
-// 
-// arma::vec h(6, arma::fill::zeros);
-// arma::vec yv;
-// 
-// for (int i=0; i < n; i++) {
-// 
-//   mu = pars(0, i);
-//   lpsi = pars(1, i);
-//   txi = pars(2, i);
-// 
-//   h.zeros();
-// 
-//   m = yc(i).size(); // number of obs
-//   yv = yc(i);
-//   
-//   for (int k=0; k < m; k++) {
-//     
-//     y = yv(k);
-//     
-//     if(arma::is_finite(y)) {
-//     
-//     ee2 = exp(-txi);
-//     ee3 = 1 + ee2;
-//     ee4 = 1.5/ee3;
-//     ee5 = ee4 - 1;
-//     ee6 = exp(lpsi);
-//     ee7 = y - mu;
-//     ee9 = ee5 * ee7/ee6;
-//     ee10 = ee9 + 1;
-//     ee11 = 1/ee5;
-//     ee12 = R_pow(ee10, ee11);
-//     ee13 = 1 + ee11;
-//     ee14 = ee10 * ee6;
-//     ee15 = R_pow(ee3, 2);
-//     ee16 = log1p(ee9);
-//     ee17 = 1/ee12;
-//     ee18 = R_pow(ee10, ee13);
-//     ee20 = ee3 * ee5;
-//     ee21 = 1 + ee17;
-//     ee22 = 1.5 * (ee16/(ee12 * ee5));
-//     ee23 = 1.5/ee12;
-//     ee25 = (((ee23 - 1.5 * ee5) * ee7/ee14 + 1.5) * ee13 - (1.5 +  ee22)/ee5)/ee10 * ee2;
-//     ee27 = ((4.5/ee20 - 3) * ee2/ee3 + 1.5) * ee16;
-//     ee28 = ee13 * ee5;
-//     ee29 = ee13 * ee7;
-//     ee30 = ee15 * ee6;
-//     ee31 = R_pow(ee5, 2);
-//     ee33 = 1.5 * (ee7/(ee18 * ee6));
-//     ee34 = ee4 - ee21;
-//     
-//     h(0) +=  - (ee13 * ee34 * ee5/(R_pow(ee10, 2) * R_pow(ee6, 2)));
-//     h(1) += (((ee21 - ee4) * ee7/ee14 + 1) * ee13 * ee5 - ee17)/ee10/ee6;
-//     h(2) += -(ee25/ee30);
-//     h(3) += -(((ee34 * ee7/ee14 - 1) * ee13 * ee5 + ee17)/ee10 * ee7/ee6);
-//     h(4) += -(ee25 * ee7/ee30);
-//     h(5) += (((((2.25/ee20 - 3) * ee2/ee3 + 1.5)/ee18 - 1.5 * ((1.5 * (ee16/(ee18 * ee31)) -
-//       1.5 * (ee29/(R_pow(ee10, ee11 +
-//       2) * ee6))) * ee2/ee15)) * ee7/ee6 + (ee27 + (1.5 * ((ee22 -
-//       ee33) * ee16/ee5) - 2.25 * (ee7/ee14)) * ee2/ee15 + (2.25 * (ee2 * ee7/(ee10 * ee15 * ee6)) -
-//       ee27)/ee12)/ee5)/ee5 -
-//       (((2.25 * (ee7/(ee10 * ee3 * ee6)) - 3) * ee2/ee3 + 1.5) * ee13 +
-//       2.25 * (ee2/(ee15 * ee31))) * ee7/ee14) * ee2/ee15;
-//     
-//     }
-//     
-//   }
-// 
-//   H(i, i) = h(0);
-//   H(i + n, i) = h(1);
-//   H(i, i + n) = h(1);
-//   H(i + 2 * n, i) = h(2);
-//   H(i, i + 2 * n) = h(2);
-//   H(i + n, i + n) = h(3);
-//   H(i + n, i + 2 * n) = h(4);
-//   H(i + 2 * n, i + n) = h(4);
-//   H(i + 2 * n, i + 2 * n) = h(5);
-//   
-//   // H(id(0), id(0)) = h(0);
-//   // H(id(1), id(0)) = h(1);
-//   // H(id(2), id(0)) = h(2);
-//   // H(id(0), id(1)) = h(1);
-//   // H(id(1), id(1)) = h(3);
-//   // H(id(2), id(1)) = h(4);
-//   // H(id(0), id(2)) = h(2);
-//   // H(id(1), id(2)) = h(4);
-//   // H(id(2), id(2)) = h(5);
-//   // 
-//   // id += 1;
-// 
-// }
-// 
-// return H;
-// 
-// }
-// 
-// // [[Rcpp::export(.tgevgmrfld2mat)]]
-// arma::mat tgevgmrfld2mat(const arma::mat& pars, const arma::field<arma::vec>& yc)
-// {
-//   
-//   int n = yc.n_rows; // number of locations
-//   int m;
-//   
-//   arma::mat H(n, 6, arma::fill::zeros);
-//   
-//   double y, mu, lpsi, txi;
-//   
-//   double ee2, ee3, ee4, ee5, ee6, ee7, ee9;
-//   double ee10, ee11, ee12, ee13, ee14, ee15, ee16, ee17, ee18;
-//   double ee20, ee21, ee22, ee23, ee25, ee27, ee28, ee29;
-//   double ee30, ee31, ee33, ee34;
-//   
-//   arma::vec yv;
-//   
-//   for (int i=0; i < n; i++) {
-//     
-//     mu = pars(0, i);
-//     lpsi = pars(1, i);
-//     txi = pars(2, i);
-// 
-//     m = yc(i).size(); // number of obs
-//     yv = yc(i);
-//     
-//     for (int k=0; k < m; k++) {
-//       
-//       y = yv(k);
-//       
-//       ee2 = exp(-txi);
-//       ee3 = 1 + ee2;
-//       ee4 = 1.5/ee3;
-//       ee5 = ee4 - 1;
-//       ee6 = exp(lpsi);
-//       ee7 = y - mu;
-//       ee9 = ee5 * ee7/ee6;
-//       ee10 = ee9 + 1;
-//       ee11 = 1/ee5;
-//       ee12 = R_pow(ee10, ee11);
-//       ee13 = 1 + ee11;
-//       ee14 = ee10 * ee6;
-//       ee15 = R_pow(ee3, 2);
-//       ee16 = log1p(ee9);
-//       ee17 = 1/ee12;
-//       ee18 = R_pow(ee10, ee13);
-//       ee20 = ee3 * ee5;
-//       ee21 = 1 + ee17;
-//       ee22 = 1.5 * (ee16/(ee12 * ee5));
-//       ee23 = 1.5/ee12;
-//       ee25 = (((ee23 - 1.5 * ee5) * ee7/ee14 + 1.5) * ee13 - (1.5 +  ee22)/ee5)/ee10 * ee2;
-//       ee27 = ((4.5/ee20 - 3) * ee2/ee3 + 1.5) * ee16;
-//       ee28 = ee13 * ee5;
-//       ee29 = ee13 * ee7;
-//       ee30 = ee15 * ee6;
-//       ee31 = R_pow(ee5, 2);
-//       ee33 = 1.5 * (ee7/(ee18 * ee6));
-//       ee34 = ee4 - ee21;
-//       
-//       H(i, 0) +=  - (ee13 * ee34 * ee5/(R_pow(ee10, 2) * R_pow(ee6, 2)));
-//       H(i, 1) += (((ee21 - ee4) * ee7/ee14 + 1) * ee13 * ee5 - ee17)/ee10/ee6;
-//       H(i, 2) += -(ee25/ee30);
-//       H(i, 3) += -(((ee34 * ee7/ee14 - 1) * ee13 * ee5 + ee17)/ee10 * ee7/ee6);
-//       H(i, 4) += -(ee25 * ee7/ee30);
-//       H(i, 5) += (((((2.25/ee20 - 3) * ee2/ee3 + 1.5)/ee18 - 1.5 * ((1.5 * (ee16/(ee18 * ee31)) -
-//         1.5 * (ee29/(R_pow(ee10, ee11 +
-//         2) * ee6))) * ee2/ee15)) * ee7/ee6 + (ee27 + (1.5 * ((ee22 -
-//         ee33) * ee16/ee5) - 2.25 * (ee7/ee14)) * ee2/ee15 + (2.25 * (ee2 * ee7/(ee10 * ee15 * ee6)) -
-//         ee27)/ee12)/ee5)/ee5 -
-//         (((2.25 * (ee7/(ee10 * ee3 * ee6)) - 3) * ee2/ee3 + 1.5) * ee13 +
-//         2.25 * (ee2/(ee15 * ee31))) * ee7/ee14) * ee2/ee15;    
-//       
-//     }
-//     
-//     // H(i, i) = h(0);
-//     // H(i + n, i) = h(1);
-//     // H(i, i + n) = h(1);
-//     // H(i + 2 * n, i) = h(2);
-//     // H(i, i + 2 * n) = h(2);
-//     // H(i + n, i + n) = h(3);
-//     // H(i + n, i + 2 * n) = h(4);
-//     // H(i + 2 * n, i + n) = h(4);
-//     // H(i + 2 * n, i + 2 * n) = h(5);
-//     // 
-//     // H(id(0), id(0)) = h(0);
-//     // H(id(1), id(0)) = h(1);
-//     // H(id(2), id(0)) = h(2);
-//     // H(id(0), id(1)) = h(1);
-//     // H(id(1), id(1)) = h(3);
-//     // H(id(2), id(1)) = h(4);
-//     // H(id(0), id(2)) = h(2);
-//     // H(id(1), id(2)) = h(4);
-//     // H(id(2), id(2)) = h(5);
-//     // 
-//     // id += 1;
-//     
-//   }
-//   
-//   return H;
-//   
-// }
 
 // [[Rcpp::export(.tgevgmrfld12)]]
 arma::mat tgevgmrfld12(arma::mat pars, arma::field<arma::vec> yc, arma::field<arma::vec> wc)
@@ -403,7 +100,7 @@ arma::mat tgevgmrfld12(arma::mat pars, arma::field<arma::vec> yc, arma::field<ar
       y = yv(k);
       w = wv(k);
       
-      if(arma::is_finite(y)) {
+      if(std::isfinite(y)) {
       
       ee2 = exp(-txi);
       ee3 = 1 + ee2;
@@ -497,7 +194,7 @@ arma::mat tgevgmrfldJ(arma::mat pars, arma::field<arma::vec> yc, arma::field<arm
       y = yv(k);
       w = wv(k);
       
-      if(arma::is_finite(y)) {
+      if (std::isfinite(y)) {
         
         ee2 = exp(-txi);
         ee3 = 1 + ee2;
@@ -567,6 +264,85 @@ arma::mat tgevgmrfldJ(arma::mat pars, arma::field<arma::vec> yc, arma::field<arm
   
 }
 
+// [[Rcpp::export(.tgevgmrfldJarr)]]
+arma::cube tgevgmrfldJarr(arma::mat pars, arma::field<arma::vec> yc, arma::field<arma::vec> wc)
+{
+  
+  int n = yc.n_rows; // number of locations
+  int m = yc(0).size();
+  
+  double y, w, mu, lpsi, txi, xi;
+  
+  arma::cube g = arma::cube(n, m, 3, arma::fill::zeros);
+  
+  double ee2, ee3, ee4, ee5, ee6, ee7, ee9;
+  double ee10, ee11, ee12, ee13, ee14, ee15, ee16, ee17, ee18;
+  double ee20, ee21, ee22, ee23, ee25, ee27, ee28, ee29;
+  double ee30, ee31, ee33, ee34;
+  
+  arma::vec yv, wv;
+  
+  for (int j=0; j < n; j++) {
+    
+    mu = pars(0, j);
+    lpsi = pars(1, j);
+    txi = pars(2, j);
+    
+    m = yc(j).size(); // number of obs
+    yv = yc(j);
+    wv = wc(j);
+    
+    for (int k=0; k < m; k++) {
+      
+      y = yv(k);
+      w = wv(k);
+      
+      if (std::isfinite(y)) {
+        
+        ee2 = exp(-txi);
+        ee3 = 1 + ee2;
+        ee4 = 1.5/ee3;
+        ee5 = ee4 - 1;
+        ee6 = exp(lpsi);
+        ee7 = y - mu;
+        ee9 = ee5 * ee7/ee6;
+        ee10 = ee9 + 1;
+        ee11 = 1/ee5;
+        ee12 = R_pow(ee10, ee11);
+        ee13 = 1 + ee11;
+        ee14 = ee10 * ee6;
+        ee15 = R_pow(ee3, 2);
+        ee16 = log1p(ee9);
+        ee17 = 1/ee12;
+        ee18 = R_pow(ee10, ee13);
+        ee20 = ee3 * ee5;
+        ee21 = 1 + ee17;
+        ee22 = 1.5 * (ee16/(ee12 * ee5));
+        ee23 = 1.5/ee12;
+        ee25 = (((ee23 - 1.5 * ee5) * ee7/ee14 + 1.5) * ee13 - (1.5 +  ee22)/ee5)/ee10 * ee2;
+        ee27 = ((4.5/ee20 - 3) * ee2/ee3 + 1.5) * ee16;
+        ee28 = ee13 * ee5;
+        ee29 = ee13 * ee7;
+        ee30 = ee15 * ee6;
+        ee31 = R_pow(ee5, 2);
+        ee33 = 1.5 * (ee7/(ee18 * ee6));
+        ee34 = ee4 - ee21;
+        
+        g(j, k, 0) = w * (-((ee28 - ee17)/ee14));
+        g(j, k, 1) = w * ((ee17 - ee28) * ee7/ee14 + 1);
+        g(j, k, 2) = w * ((((ee23 - 1.5) * ee16/ee5 - ee33)/ee5 + 1.5 * (ee29/
+          ee14)) * ee2/ee15);
+
+      }
+      
+    }
+    
+  }
+  
+  return g;
+  
+}
+
 // [[Rcpp::export(.tgevgmrfld0_omp)]]
 double tgevgmrfld0_omp(arma::mat pars, arma::field<arma::vec> yc, arma::field<arma::vec> wc, int threads = 0)
 {
@@ -622,7 +398,7 @@ double tgevgmrfld0_omp(arma::mat pars, arma::field<arma::vec> yc, arma::field<ar
       y = yv(k);
       w = wv(k);
       
-      if(arma::is_finite(y)) {
+      if(std::isfinite(y)) {
         
         ee1 = xi * (y - mu) / exp(lpsi);
         
@@ -726,7 +502,7 @@ arma::mat tgevgmrfld12_omp(arma::mat pars, arma::field<arma::vec> yc, arma::fiel
       y = yv(k);
       w = wv(k);
       
-      if(arma::is_finite(y)) {
+      if(std::isfinite(y)) {
         
         ee2 = exp(-txi);
         ee3 = 1 + ee2;
